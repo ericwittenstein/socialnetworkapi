6e3139b8bbe895f84ab1974d4b7c2bd652024a9e
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-08-18 19:23:45 -0400

    working on thought controllers, dinner break

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,40 +1,41 @@
-const { Post, User } = require('../models');
+const { Thought, User } = require("../models");
 
 module.exports = {
-  getPosts(req, res) {
-    Post.find()
-      .then((posts) => res.json(posts))
-      .catch((err) => res.status(500).json(err));
-  },
-  getSinglePost(req, res) {
-    Post.findOne({ _id: req.params.postId })
-      .then((post) =>
-        !post
-          ? res.status(404).json({ message: 'No post with that ID' })
-          : res.json(post)
-      )
-      .catch((err) => res.status(500).json(err));
-  },
-  // create a new post
-  createPost(req, res) {
-    Post.create(req.body)
-      .then((post) => {
-        return User.findOneAndUpdate(
-          { _id: req.body.userId },
-          { $addToSet: { posts: post._id } },
-          { new: true }
-        );
-      })
-      .then((user) =>
-        !user
-          ? res
-              .status(404)
-              .json({ message: 'Post created, but found no user with that ID' })
-          : res.json('Created the post 🎉')
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  },
+	getThoughts(req, res) {
+		Thought.find()
+			.then((thoughts) => res.json(thoughts))
+			.catch((err) => res.status(500).json(err));
+	},
+	getOneThought(req, res) {
+		Thought.findOne({ _id: req.params.thoughtId })
+			.populate({ path: "reactions", select: "-__v" })
+			.then((thought) =>
+				!thought
+					? res.status(404).json({
+							message:
+								"no thoughts, head empty, id just wind over ocean",
+					  })
+					: res.json(thought)
+			)
+			.catch((err) => res.status(500).json(err));
+	},
+	// create a new thought
+	createThought(req, res) {
+		Thought.create(req.body)
+			.then((thoughtDB) => res.json(thoughtDB))
+			.catch((err) => res.status(500).json(err));
+	},
+	// update a thought
+	updateThought(req, res) {
+		Thought.updateOne({ _id: req.params.thoughtId }, req.body)
+			.then((thought) =>
+				!thought
+					? res.status(404).json({
+							message:
+								"no thoughts, head empty, id just wind over ocean",
+					  })
+					: res.json(thought)
+			)
+			.catch((err) => res.status(500).json(err));
+	},
 };
